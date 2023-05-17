80c0f11b3d14ebd244a89560332d2399005bfa9d
Willian Giacomelli <willian2406@live.com>
2023-05-17 17:26:14 -0300

    feat(creation of movies): creation of movies only allowed for admin logged

--- src/routes/movies.routes.js
+++ src/routes/movies.routes.js
@@ -4,12 +4,19 @@ const moviesController = require("../controllers/movies.controller");
 
 const { verifyAuthenticate } = require("../middlewares/verifyAuthentication");
 
+const verifyUserRole = require("../middlewares/verifyUserRole");
+
 const routes = Router();
 
 routes.get("/movies", moviesController.list);
 routes.get("/movies/:id", moviesController.getById);
 
-routes.post("/movies", verifyAuthenticate, moviesController.create);
+routes.post(
+  "/movies",
+  verifyAuthenticate,
+  verifyUserRole,
+  moviesController.create
+);
 
 routes.put("/movies/:id", moviesController.update);
 
